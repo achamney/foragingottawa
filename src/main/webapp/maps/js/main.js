5cae36f302cb03e0b1305d89a33cc9b329e2c1c1
Austin <achamney@gmail.com>
2015-12-26 16:27:14 -0500

    added recent manager

--- src/main/webapp/maps/js/main.js
+++ src/main/webapp/maps/js/main.js
@@ -3,9 +3,11 @@ require(['/quickforms/js/quickforms.js'],function(){
 	quickforms.registerReadyFunction(function()
 	{
 		require(['esri/map','foragingottawa/DataManager', 'foragingottawa/FormManager',
+                'foragingottawa/RecentManager',
 				'server/getFactData','dom/form/text','dom/form/date'],
-		function(Map, DataManager, FormManager){
+		function(Map, DataManager, FormManager, RecentManager){
 			var dm = new DataManager();
+            var recentManager = new RecentManager();
 			var formManager = new FormManager('addPointForm', dm);
 			var map = new Map("mainMap", {
 				center: [-75.696188,45.419714],
@@ -16,6 +18,7 @@ require(['/quickforms/js/quickforms.js'],function(){
 			map.on("load", function() {
 				dm.getLayer('getForageLocations').then(function(layer){
 					map.addLayer(layer);
+                    recentManager.renderRecent(layer.graphics);
 				});
 			});
 			
@@ -23,12 +26,25 @@ require(['/quickforms/js/quickforms.js'],function(){
 				dm.clickPoint(e);
 				var projected = Terraformer.ArcGIS.parse(e.mapPoint);
 				formManager.setLatLong(projected);
+                
 			});
 			
 			quickforms.parseForm( //formId*, app, fact*, callback
 							{formId:'addPointForm',
 							fact:'foragelocations'});
-
+                            
+            $('#mapTabs #recentTab').click(function(){
+                $(this).addClass("active");
+                $('#addLocationTab').removeClass("active");
+                $('#recentView').show();
+                $('#addLocationView').hide();
+            });
+            $('#mapTabs #addLocationTab').click(function(){
+                $(this).addClass("active");
+                $('#recentTab').removeClass("active");
+                $('#recentView').hide();
+                $('#addLocationView').show();
+            });
 		});
 	});
 });
