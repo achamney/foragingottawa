3f9054e08e51de1de77c65f80f9bd82f344e3cd5
achamney <achamney@gmail.com>
2017-03-14 07:47:30 -0400

    map now updates coordinates

--- src/main/webapp/app/maps/gmap.js
+++ src/main/webapp/app/maps/gmap.js
@@ -2,21 +2,39 @@ define([], function() {
     var component = ng.core.Component({
         selector: 'map',
         template: '',
-        inputs: ['data']
+        inputs: ['data'],
+        outputs: ['newMarker']
     }).Class({
         
         constructor: function (ref) {
             this.ref = ref.nativeElement;
+            this.newMarker = new ng.core.EventEmitter(); 
         },
         
         ngOnInit: function () {
             if(!this.mapImpl) {
                 $(this.map).css({ "height": 300, "width":"100%" });
                 var coords = { lat: 45.3768482, lng: -75.7100154 };
+                var _this = this;
                 this.mapImpl = new google.maps.Map(this.ref, {
                     zoom: 12,
                     center: coords,
                     scrollwheel: false
+                });
+                var tempMarker = null;
+                this.mapImpl.addListener("click", function(e) {
+                    if(tempMarker) {
+                        tempMarker.setMap(null);
+                    }
+                    tempMarker = new google.maps.Marker({
+                        position: { lat: e.latLng.lat(), lng: e.latLng.lng() },
+                        map: _this.mapImpl,
+                        title: 'Hello World!'
+                    });
+                    _this.newMarker.next(e.latLng);
+                });
+                var infowindow = new google.maps.InfoWindow({
+                    content: "<h1>HelloWorld</h1>"
                 });
                 if (this.data) {
                     for(var datum of this.data) {
@@ -24,6 +42,9 @@ define([], function() {
                             position: { lat: datum.lat, lng: datum.long },
                             map: this.mapImpl,
                             title: 'Hello World!'
+                        });
+                        marker.addListener('click', function() {
+                            infowindow.open(_this.mapImpl, marker);
                         });
                     }
                 }
--- src/main/webapp/app/maps/map.js
+++ src/main/webapp/app/maps/map.js
@@ -1,5 +1,5 @@
-var hasLoaded = false;
-define(['server/getFactData','dom/form/text'],
+var hasMapLoaded = false;
+define(['server/getFactData','dom/form/text', 'dom/form/date'],
                 function(){
     return ng.core.Component({
         selector: 'maps',
@@ -7,13 +7,49 @@ define(['server/getFactData','dom/form/text'],
     }).Class({
 
         constructor: function () {
-            if(!hasLoaded) {
+            if(!hasMapLoaded) {
                 quickforms.loadCss('app/maps/css/map.css');
-                hasLoaded = true;
+                hasMapLoaded = true;
             }
 
         },
         ngOnInit: function () {
+            window.setTimeout(function() {
+                quickforms.parseForm( //formId*, app, fact*, callback
+                                {formId:'addPointForm',
+                                fact:'forageLocations'});
+            }, 200);
+            this.username = getCookie("username");
+        },
+        setPosition: function (latLng) { 
+            var lat = quickforms.currentFormaddPointForm.childMap['addPointLat'],
+                long = quickforms.currentFormaddPointForm.childMap['addPointLong'];
+            lat.currentVal=latLng.lat();
+            long.currentVal=latLng.lng();
+            lat.dom.val(lat.currentVal);
+            long.dom.val(long.currentVal);
+        },
+        submit: function(btn) {
+            var redir = quickforms.formRedirect;
+            var _this = this;
+            quickforms.formRedirect = this.onFinish.bind(this);
+            quickforms.currentFormaddPointForm.childMap['token'].currentVal = getCookie('token');
+            quickforms.putFact(btn, "/");
+            quickforms.formRedirect = redir;
+            quickforms.serverQueries[quickforms.serverQueries.length-1].addErrorListener(function(e) {
+                _this.onError(e);
+            });
+            this.loading = true;
+        },
+        
+        onFinish(data) {
+            if (data.indexOf("[") !== 0) return this.onError(data);
+            window.location="#?page=2";
+        }, 
+        onError: function(data) {
+            if(~data.indexOf("Duplicate entry")) {
+                this.duplicate = true;
+            }
         }
     });
 });
