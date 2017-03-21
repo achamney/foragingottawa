06c5dedf91a616c7ea6aeee647995c4b7534b2cf
achamney <achamney@gmail.com>
2017-03-20 22:06:20 -0400

    functional profile

--- src/main/webapp/app/profile.js
+++ src/main/webapp/app/profile.js
@@ -12,12 +12,9 @@ define([
         
         ngOnInit: function () {
             this.id = QueryString("id");
+            this.username = getCookie("username");
             this.user = { id: this.id };
-            /*window.setTimeout(function() {
-                quickforms.parseForm( //formId*, app, fact*, callback
-                                {formId:'formTemplate',
-                                fact:'users'});
-            }, 200);*/
+            
             quickforms.getFactData({
                 queryName: 'users_get_row',
                 params: 'id='+this.id,
@@ -25,11 +22,24 @@ define([
             });
         },
 
-        submitForm: function (el, captcha) {
-            if(!this.captchad) return;
+        onEdit: function() {
+            this.edit=true;
+            window.setTimeout(function() {
+                quickforms.parseForm( //formId*, app, fact*, callback
+                                {formId:'editProfileForm',
+                                fact:'users'});
+            }, 200);
+        },
+
+        submitForm: function (el) {
             var redir = quickforms.formRedirect;
             var _this = this;
             quickforms.formRedirect = this.onFinish.bind(this);
+            quickforms.currentFormeditProfileForm.childMap['token'].currentVal = getCookie('token');
+            var pass1 = quickforms.currentFormeditProfileForm.childMap['password1'].currentVal;
+            if(pass1 && pass1 === quickforms.currentFormeditProfileForm.childMap['confirmPass'].currentVal) {
+                quickforms.currentFormeditProfileForm.childMap['password'].currentVal = pass1;
+            }
             quickforms.putFact(el, "/");
             quickforms.formRedirect = redir;
             quickforms.serverQueries[quickforms.serverQueries.length-1].addErrorListener(function(e) {
@@ -43,17 +53,13 @@ define([
             if(isJSONString(data)) {
                 var json = JSON.parse(data);
                 this.user = json[0];
+                this.user.avatar = this.user.avatar === "null" ? null : this.user.avatar;
             }
         },
 
         onFinish: function(data) {
             if (data.indexOf("[") !== 0) return this.onError(data);
-            var username = quickforms.currentFormformTemplate.children.filter(function(c){return c && c.id==="username"})[0];
-            setCookie("username", username.currentVal);
-            var dataJson = JSON.parse(data);
-            setCookie("token", dataJson[0].token);
-            window.location.href="/";
-            this.loading = false;
+            window.location.reload();
         },
 
         onError: function(data) {
